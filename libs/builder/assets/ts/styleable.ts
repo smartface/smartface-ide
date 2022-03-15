6241e7aab8fd2b63c92a56c6465004aa37e6fcd9
Furkan Arabacı <furkan.arabaci@smartface.io>
2022-03-15 12:48:00 +0300

    convert requires to import on every places

--- libs/builder/assets/ts/styleable.ts
+++ libs/builder/assets/ts/styleable.ts
@@ -1,50 +1,50 @@
-import NavigationController = require('@smartface/native/ui/navigationcontroller');
-import Page = require('@smartface/native/ui/page');
-import View = require('@smartface/native/ui/view');
+import NavigationController from '@smartface/native/ui/navigationcontroller';
+import Page from '@smartface/native/ui/page';
+import View from '@smartface/native/ui/view';
 
 export declare abstract class Styleable {
-    static $$styleContext: ComponentStyleContext;
+  static $$styleContext: ComponentStyleContext;
 }
 
 type ViewType = View | NavigationController;
 
 export type ComponentStyleContext = {
-    classNames: string,
-    defaultClassNames: string,
-    userProps: { [key: string]: any },
-    statusBar?: {
-        classNames: string,
-        defaultClassNames: string,
-        userProps: { [key: string]: any }
-    },
-    headerBar?: {
-        classNames: string,
-        defaultClassNames: string,
-        userProps: { [key: string]: any }
-    }
-}
+  classNames: string;
+  defaultClassNames: string;
+  userProps: { [key: string]: any };
+  statusBar?: {
+    classNames: string;
+    defaultClassNames: string;
+    userProps: { [key: string]: any };
+  };
+  headerBar?: {
+    classNames: string;
+    defaultClassNames: string;
+    userProps: { [key: string]: any };
+  };
+};
 
 export abstract class StyledPage extends Page {
-    private children: Map<string, ViewType>;
+  private children: Map<string, ViewType>;
 
-    static $$styleContext: ComponentStyleContext;
+  static $$styleContext: ComponentStyleContext;
 
-    constructor(params: any) {
-        super(params);
-        this.children = new Map();
-        this.addChild('statusBar', this.statusBar);
-        this.addChild('headerBar', this.headerBar);
-        if (this.ios) {
-            this.ios.safeAreaLayoutMode = true;
-        }
+  constructor(params: any) {
+    super(params);
+    this.children = new Map();
+    this.addChild('statusBar', this.statusBar);
+    this.addChild('headerBar', this.headerBar);
+    if (this.ios) {
+      this.ios.safeAreaLayoutMode = true;
     }
+  }
 
-    protected addChild(name: string, child: Object) {
-        this.children.set(name, child);
-        if (this.layout) {
-            this.layout.addChild(child);
-        } else {
-            this.addChild(child);
-        }
+  protected addChild(name: string, child: Object) {
+    this.children.set(name, child);
+    if (this.layout) {
+      this.layout.addChild(child);
+    } else {
+      this.addChild(child);
     }
+  }
 }
--- libs/builder/src/core/templateHelpers/js/index.js
+++ libs/builder/src/core/templateHelpers/js/index.js
@@ -1,55 +1,56 @@
-const path = require("path");
+const path = require('path');
 
-const util = require("../../../util");
-const getPath = require("../../../config").getPath;
+const util = require('../../../util');
+const getPath = require('../../../config').getPath;
 const { REPEATED_VIEW, WITH_EXTEND, IRREGULAR_ENUMS } = require('../common');
 
 function getRequiredIrregularEnums(smfObjects) {
   var mdls = {},
     res = [];
 
-  if (arguments[1])
-    smfObjects = [arguments[1].data.root].concat(smfObjects || []);
-  if (!smfObjects)
-    return "";
+  if (arguments[1]) smfObjects = [arguments[1].data.root].concat(smfObjects || []);
+  if (!smfObjects) return '';
 
   function searchAndSet(_smfObjects) {
     _smfObjects.forEach(item => {
-      item.attributes && item.attributes.align && (mdls["ScrollViewAlign"] = true);
-      item.html && (mdls["propFactory"] = true);
-      item.html && (mdls["AttributedString"] = true);
-      item.html && (mdls["createAttributedStrings"] = true);
-      item.type === "GridView" && (mdls["LayoutManager"] = true);
-      REPEATED_VIEW[item.type] && (mdls["actionAddChild"] = true);
-      item.smfObjects && (searchAndSet(item.smfObjects));
+      item.attributes && item.attributes.align && (mdls['ScrollViewAlign'] = true);
+      item.html && (mdls['propFactory'] = true);
+      item.html && (mdls['AttributedString'] = true);
+      item.html && (mdls['createAttributedStrings'] = true);
+      item.type === 'GridView' && (mdls['LayoutManager'] = true);
+      REPEATED_VIEW[item.type] && (mdls['actionAddChild'] = true);
+      item.smfObjects && searchAndSet(item.smfObjects);
     });
   }
 
   searchAndSet(smfObjects);
 
   for (var mdl in mdls) {
-    res.push(`const ${mdl} = ${WITH_EXTEND[mdl] ? "extend(" : ""}require("${IRREGULAR_ENUMS[mdl]}")${WITH_EXTEND[mdl] ? ")" : ""};`);
+    res.push(
+      `const ${mdl} = ${WITH_EXTEND[mdl] ? 'extend(' : ''}require("${IRREGULAR_ENUMS[mdl]}")${
+        WITH_EXTEND[mdl] ? ')' : ''
+      };`
+    );
   }
-  return res.join("\n");
+  return res.join('\n');
 }
 
 function getRequiredLibModules(smfObjects, isLib) {
   var res = [],
     requireLibdModules = {},
     requiredModules = {};
-  if (!smfObjects)
-    return "";
+  if (!smfObjects) return '';
 
   function searchAndSet(_smfObjects) {
     _smfObjects.forEach(item => {
       item.isLibraryComponent && (requireLibdModules[item.libraryType] = true);
       item.isModuleComp && (requiredModules[item.moduleName] = item.modulePath);
-      item.smfObjects && (searchAndSet(item.smfObjects));
+      item.smfObjects && searchAndSet(item.smfObjects);
     });
   }
 
   searchAndSet(smfObjects);
-  var libraryPath = path.relative(getPath("SCRIPTS_FOLDER"), getPath("LIBRARY_USER_FOLDER"));
+  var libraryPath = path.relative(getPath('SCRIPTS_FOLDER'), getPath('LIBRARY_USER_FOLDER'));
   for (var mdl in requireLibdModules) {
     res.push(`const ${mdl} = extend(require("${libraryPath}/${mdl}"));`);
   }
@@ -58,7 +59,7 @@ function getRequiredLibModules(smfObjects, isLib) {
     res.push(`const ${mdl} = extend(require("${requiredModules[mdl]}"));`);
   }
 
-  return res.join("\n");
+  return res.join('\n');
 }
 
 module.exports = function(_compiler) {
