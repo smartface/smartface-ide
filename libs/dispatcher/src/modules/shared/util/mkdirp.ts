f81115abe1cb557995cdf0e1a162c854b09dfc8a
Ali Tuğrul Pınar <altgrlpinar@gmail.com>
2022-05-23 17:54:39 +0300

    fix(dispatcher): mkdirp util function

--- libs/dispatcher/src/modules/shared/util/mkdirp.ts
+++ libs/dispatcher/src/modules/shared/util/mkdirp.ts
@@ -1,16 +1,17 @@
-import  { execSync, exec } from 'child_process';
+const fse = require('fs-extra')
 
 export const mkdirpSync = (dest: string) => {
-    return execSync(`mkdir -p ${dest}`);
+    return fse.ensureDirSync(dest);
 };
 
 export const mkdirpAsync = (dest: string) => {
     return new Promise((resolve, reject) => {
-        exec(`mkdir -p ${dest} `, (error, stdout, stderr) => {
-            if (error || stderr)
-                reject(error || stderr);
-            else
-                resolve(stdout);
+        fse.ensureDir(dest, err => {
+            if(err){
+                reject(err);
+            }else{
+                resolve(null);
+            }
         });
     });
 };
